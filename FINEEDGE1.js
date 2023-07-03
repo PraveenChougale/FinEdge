39e85c0bf4ce51a78c76b7148f7c3812c8c0cfe2
PraveenChougale <130298073+PraveenChougale@users.noreply.github.com>
2023-07-03 14:23:19 +0530

    Add files via upload

---
+++ FINEEDGE1.js
@@ -0,0 +1,10 @@
+
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    
+  });
+  
